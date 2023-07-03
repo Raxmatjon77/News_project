09734a886bded78a29567ac360cf7bdbe39b3028
raxmatjon <raxmatjonhamidov242@gmail.com>
2023-07-03 22:25:09 +0500

    requirements.txt added

--- news_project/settings.py
+++ news_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from pickle import TRUE
 
@@ -142,8 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
-STATIC_ROOT=BASE_DIR/'staticfiles'
+# STATICFILES_DIRS=[BASE_DIR/'static']
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+# STATIC_ROOT=BASE_DIR/'staticfiles'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_FINDERS=[
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
